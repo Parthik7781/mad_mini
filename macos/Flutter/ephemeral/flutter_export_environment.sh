4bf433c6e6eecfbf80ea83d14b50fed7b4a31a08
Parthik7781 <97076828+Parthik7781@users.noreply.github.com>
2024-05-08 23:32:53 +0530

    Add files via upload

---
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=C:\sem_6\flutter_windows_3.16.9-stable\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\parth\Downloads\MAD\note_app-main\note_app-main"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=0.1.0"
+export "FLUTTER_BUILD_NUMBER=0.1.0"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
